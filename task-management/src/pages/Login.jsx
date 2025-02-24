8fc79b105015b98f348f9243b27fb4834018118b
devam-j-eq <devam.jasani-i@equestsolutions.net>
2025-02-24 18:03:12 +0530

    create login page

--- task-management/src/pages/Login.jsx
+++ task-management/src/pages/Login.jsx
@@ -1,36 +1,108 @@
 import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import Textbox from '../components/Textbox';
+import Button from '../components/Button';
+import { Instagram, Linkedin, Facebook, Globe } from 'lucide-react';
 
 const Login = () => {
-    const user = "";
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        }= useForm();
+  const user = "";
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        user && navigate("/Dashboard")
-    }, [user]);
-     
-  return ( 
+  const submitHandler = async (data) => {
+    console.log("submit");  
+  };
+
+  useEffect(() => {
+    user && navigate("/Dashboard");
+  }, [user]);
+
+  return (
     <div className='w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]'>
-        <div className='w-full md:w-auto flex gap-40 flex-col md:flex-row items-center justify-center'>
-            {/*leftside*/} 
-            <div className='h-full w-full lg:w-2/3 flex flex-col items-center justify-center'>
-                <div className='w-full h-full md:max-w-lg 2xl:max:w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20'>
-                    <span className='flex items-center justify-center gap-1 py-1 px-3 border rounded-full text-sm md:text-base border-gray-300 text-gray-600'>
-                        Manage all your tasks in one place
-                    </span>
-                </div>
+      <div className='w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center'>
+        {/* left side */}
+        <div className='h-full w-full lg:w-2/3 flex flex-col items-center justify-center'>
+          <div className='w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20'>
+            <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base border-gray-300 text-gray-600'>
+              Manage all your tasks in one place!
+            </span>
+            <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700'>
+              <span>eQuest Solutions</span>
+              <span>Task Management</span>
+            </p>
+            <div className='cell'>
+              <div className='circle rotate-in-up-left'></div>
             </div>
-
-            {/* right side */}
-            <div></div>
+          </div>
         </div>
+
+        {/* right side */}
+        <div className='w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center'>
+          <form onSubmit={handleSubmit(submitHandler)}
+          className='form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white px-10 pt-14 pb-14'
+          >
+            <div className=''>
+              <p className='text-blue-600 text-3xl font-bold text-center'>Welcome Back!</p>
+              <p className='text-center text-base text-gray-700'>Keep all your task safe</p>
+            </div> 
+
+            <div className='flex flex-col gap-y-5'>
+              <Textbox
+              placeholder='email@example.com'
+              type='email'
+              name='email'
+              lable='Email Address'
+              className='w-full rounded-full'
+              register={register("email",{
+                required:"Email Address is requried!",
+              })}
+                error={errors.email ? errors.email.message : ""}>
+              </Textbox>
+
+              <Textbox
+              placeholder='your password'
+              type='password'
+              name='password'
+              lable='password'
+              className='w-full rounded-full'
+              register={register("password",{
+                required:"password is requried!",
+              })}
+                error={errors.password ? errors.password.message : ""}>
+              </Textbox>
+              <span className='text-sm text-gray-500 hover:text-blue-600 hover:underline cursor-pointer'>
+                Forget Password?
+              </span>
+
+              <Button
+              type='submit'
+              lable='submit'
+              className='w-full h-10 bg-blue-700 text-white rounded-full'></Button>
+              <div className='flex justify-center gap-6 mt-4'>
+              <a href='https://in.linkedin.com/company/equest-solutions-india' target='_blank' rel='noopener noreferrer' className='text-blue-400 hover:text-blue-700'>
+                  <Linkedin size={24} />
+                </a>
+                <a href='https://www.instagram.com/explore/locations/475766973/equest-solutions/?hl=en' target='_blank' rel='noopener noreferrer' className='text-purple-500 hover:text-pink-600'>
+                <Instagram size={24} />
+                </a>
+                <a href='https://www.facebook.com/equestsolutions/' target='_blank' rel='noopener noreferrer' className='text-blue-500 hover:text-blue-600'>
+                  <Facebook size={24} />
+                </a>
+                <a href='https://equestsolutions.net/' target='_blank' rel='noopener noreferrer' className='text-gray-500 hover:text-green-600'>
+                  <Globe size={24} />
+                </a>
+             </div>
+
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
