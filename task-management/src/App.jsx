cfc0f1c8ad3254f9a278093def020af20423237c
devam-j-eq <devam.jasani-i@equestsolutions.net>
2025-02-25 18:06:56 +0530

    update file

--- task-management/src/App.jsx
+++ task-management/src/App.jsx
@@ -1,4 +1,5 @@
-import { Navigate, Route, Routes, useLocation, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
 import { Toaster } from 'sonner';
 import Login from "./pages/Login";
 import TaskDetails from "./pages/TaskDetails";
@@ -6,28 +7,32 @@ import Task from "./pages/Task";
 import Trash from "./pages/Trash";
 import Users from "./pages/Users";
 import Dashboard from "./pages/Dashboard";
+import Sidebar from "./components/Sidebar";
 
-function Layout() {
-  const user = ""; 
+function LayoutWrapper() {
+  const { user } = useSelector((state) => state.auth);
   const location = useLocation();
 
-  return user ? 
-  (
+  console.log("User State:", user); // Check user state
+
+  if (user === undefined) return null; // Wait until user is defined
+
+  return user ? (
     <div className="w-full h-screen flex flex-col md:flex-row">
-      <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
-        {/* <Sidebar /> */}
+      {/* Sidebar */}
+      <div className="w-1/5 h-screen bg-white sticky top-0 block z-10">
+        <Sidebar />
       </div>
-      {/* <MobileSidebar /> */}
+
+      {/* Main Content */}
       <div className="flex-1 overflow-y-auto">
-        {/* <Navbar /> */}
         <div className="p-4 2xl:px-10">
-          <Outlet /> 
+          <Outlet />
         </div>
       </div>
     </div>
-  )
-   : (
-    <Navigate to="/log-in" state={{ from: location }} replace />
+  ) : (
+    <Navigate to="/dashboard" state={{ from: location }} replace />
   );
 }
 
@@ -35,8 +40,9 @@ function App() {
   return (
     <main className="w-full min-h-screen bg-[#f3f4f6]">
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Navigate to="/dashboard" />} />
+        <Route path="/log-in" element={<Login />} />
+        
+          <Route path="/" element={<Navigate to="/dashboard" />} /> 
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/task" element={<Task />} />
           <Route path="/completed/:status" element={<Task />} />
@@ -45,8 +51,7 @@ function App() {
           <Route path="/team" element={<Users />} />
           <Route path="/trashed" element={<Trash />} />
           <Route path="/task/:id" element={<TaskDetails />} />
-        </Route>
-        <Route path="/log-in" element={<Login />} />
+        
       </Routes>
       <Toaster richColors />
     </main>
