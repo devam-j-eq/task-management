52360a494dd3dff9a18d9d89d085e2e77e68e967
devam-j-eq <devam.jasani-i@equestsolutions.net>
2025-02-27 18:23:20 +0530

    update login page

--- task-management/src/pages/Login.jsx
+++ task-management/src/pages/Login.jsx
@@ -28,53 +28,72 @@ const Login = () => {
   return (
     <div className='w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]'>
       <div className='w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center'>
-        
-        {/* Left side */}
+        {/* left side */}
         <div className='h-full w-full lg:w-2/3 flex flex-col items-center justify-center'>
-          <div className='w-full md:max-w-lg flex flex-col items-center justify-center gap-5'>
-            <span className='py-1 px-3 border rounded-full text-sm border-gray-300 text-gray-600'>
-              Manage all your tasks in one place!
+          <div className='w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20'>
+            <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300 text-gray-600'>
+              Manage all your task in one place!
             </span>
-            <p className='text-4xl font-black text-center text-blue-700'>
-              <span>eQuest Solutions</span><br/>
-              <span>Task Management</span>
+            <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700'>
+              <span>eQuest Solutions</span>
+              <span>Task Manager</span>
             </p>
+
+            <div className='cell'>
+              <div className='circle rotate-in-up-left'></div>
+            </div>
           </div>
         </div>
 
         {/* Right side (Form) */}
-        <div className='w-full md:w-1/3 p-4 flex flex-col justify-center items-center'>
-          <form 
+        <div className='w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center'>
+        <form
             onSubmit={handleSubmit(submitHandler)}
-            className='w-full md:w-[400px] bg-white px-10 pt-14 pb-14 rounded-lg shadow-md'
+            className='form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white px-10 pt-14 pb-14'
           >
-            <p className='text-blue-600 text-3xl font-bold text-center'>Welcome Back!</p>
-            <p className='text-center text-base text-gray-700 mb-8'>Keep all your tasks safe</p>
+            <div className=''>
+              <p className='text-blue-600 text-3xl font-bold text-center'>
+                Welcome back!
+              </p>
+              <p className='text-center text-base text-gray-700 '>
+                Keep all your credential safge.
+              </p>
+            </div>
 
-            <Textbox
-              placeholder='email@example.com'
-              type='email'
-              name='email'
-              lable='Email Address'
-              register={register("email", { required: "Email Address is required!" })}
-              error={errors.email?.message}
-            />
+            <div className='flex flex-col gap-y-5'>
+              <Textbox
+                placeholder='email@example.com'
+                type='email'
+                name='email'
+                label='Email Address'
+                className='w-full rounded-full'
+                register={register("email", {
+                  required: "Email Address is required!",
+                })}
+                error={errors.email ? errors.email.message : ""}
+              />
+              <Textbox
+                placeholder='your password'
+                type='password'
+                name='password'
+                label='Password'
+                className='w-full rounded-full'
+                register={register("password", {
+                  required: "Password is required!",
+                })}
+                error={errors.password ? errors.password.message : ""}
+              />
 
-            <Textbox
-              placeholder='your password'
-              type='password'
-              name='password'
-              lable='Password'
-              register={register("password", { required: "Password is required!" })}
-              error={errors.password?.message}
-            />
+              <span className='text-sm text-gray-500 hover:text-blue-600 hover:underline cursor-pointer'>
+                Forget Password?
+              </span>
 
-            <span className='text-sm text-gray-500 hover:text-blue-600 hover:underline cursor-pointer mt-2'>
-              Forget Password?
-            </span>
-
-            <Button type='submit' lable='Submit' className='w-full h-10 bg-blue-700 text-white rounded-full mt-4' />
-
+              <Button
+                type='submit'
+                label='Submit'
+                className='w-full h-10 bg-blue-700 text-yellow-50 rounded-full'
+              />
+            </div>
             {/* Social Icons */}
             <div className='flex justify-center gap-6 mt-6'>
               <a href='https://in.linkedin.com/company/equest-solutions-india' target='_blank' rel='noopener noreferrer' className='text-blue-400 hover:text-blue-700'>
