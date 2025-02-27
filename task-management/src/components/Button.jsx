f6bc8200bbacb1284a2f80ae42b83704c66611b6
devam-j-eq <devam.jasani-i@equestsolutions.net>
2025-02-27 18:22:31 +0530

    update file

--- task-management/src/components/Button.jsx
+++ task-management/src/components/Button.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Button = ({icon, className, lable, type, onclick = () => {} }) => {
   return (
     <button
-      type={type || "button"} className={clsx("px-3 py-2 outline-none", className)}>
+      type={type || "button"} className={clsx("px-3 py-2 outline-none", className)}>Submit
     
       <span>{lable}</span>
       {icon && icon}
