fcda440eb0b835c3f0762a4d2753ed02529a48ba
devam-j-eq <devam.jasani-i@equestsolutions.net>
2025-03-06 13:53:59 +0530

    add data of chart

---
+++ task-management/src/assets/data.js
@@ -0,0 +1,51 @@
+export const chartData = [
+    {
+      name: "Week 1",
+      Completed: 2,
+      InProgress: 3,
+      ToDo: 4,
+    },
+    {
+      name: "Week 2",
+      Completed: 3,
+      InProgress: 8,
+      ToDo: 5,
+    },
+    {
+      name: "Week 3",
+      Completed: 8,
+      InProgress: 12,
+      ToDo: 9,
+    },
+    {
+      name: "Week 4",
+      Completed: 3,
+      InProgress: 4,
+      ToDo: 6,
+    },
+    {
+        name: "Week 5",
+        Completed: 2,
+        InProgress: 13,
+        ToDo: 4,
+      },
+      {
+        name: "Week 6",
+        Completed: 13,
+        InProgress: 8,
+        ToDo: 7,
+      },
+      {
+        name: "Week 7",
+        Completed: 8,
+        InProgress: 12,
+        ToDo: 9,
+      },
+      {
+        name: "Week 8",
+        Completed: 3,
+        InProgress: 4,
+        ToDo: 6,
+      },
+  ];
+  
