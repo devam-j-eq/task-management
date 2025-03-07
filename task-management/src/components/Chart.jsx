3b7e07a6a3304576969abdb49778a49d7fa5e758
devam-j-eq <devam.jasani-i@equestsolutions.net>
2025-03-07 18:33:28 +0530

    update a chart

--- task-management/src/components/Chart.jsx
+++ task-management/src/components/Chart.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import {
   Bar,
   BarChart,
@@ -9,21 +9,35 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
- import { chartData } from "../assets/data";
+import { chartData, summary } from "../assets/data";
 
 export const Chart = () => {
+  
+  const updatedChartData = useMemo(() => {
+    if (!summary || !summary.tasks) return chartData; 
+
+    const summaryWeek = {
+      name: "Total",
+      Completed: summary.tasks?.completed ?? 0,
+      InProgress: summary.tasks?.["in progress"] ?? 0,
+      ToDo: summary.tasks?.todo ?? 0,
+    };
+
+    return [...chartData, summaryWeek]; 
+  }, [summary, chartData]);
+
   return (
-    <ResponsiveContainer width={"100%"} height={300}>
-      <BarChart width={100} height={40} data={chartData}>
-        <XAxis dataKey='name'  />
-        <YAxis />
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={updatedChartData}>
+        <XAxis dataKey="name" />
+        <YAxis data />
         <Tooltip />
         <Legend />
-        <CartesianGrid strokeDasharray='3 3' />
-        
-        <Bar dataKey='Completed' fill='#0f766e' />
-        <Bar dataKey='InProgress' fill='#f59e0b' />
-        <Bar dataKey='ToDo' fill='#be185d' />
+        <CartesianGrid strokeDasharray="3 3" />
+
+        <Bar dataKey="Completed" fill="#0f766e" />
+        <Bar dataKey="InProgress" fill="#f59e0b" />
+        <Bar dataKey="ToDo" fill="#be185d" />
       </BarChart>
     </ResponsiveContainer>
   );
